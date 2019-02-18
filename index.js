e25045f80b2e73181300baf4fd67082d88fd6222
Juan Camilo Salazar <juan.salazar@impartner.com>
2019-02-18 15:31:48 -0500

    Purposal for Change: change the way to query the entrypoints, because accesing through the chunks and querying issuer, sometimes has a null value, and didn't allow the generation of d.ts file

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
-const { generateDtsBundle } = require('dts-bundle-generator');
-const loaderUtils = require('loader-utils');
+const { generateDtsBundle } = require("dts-bundle-generator");
+const loaderUtils = require("loader-utils");
 
 const runTest = (test, value) => {
   if (test instanceof RegExp) {
     return test.test(value);
-  } else if (typeof test === 'function') {
+  } else if (typeof test === "function") {
     return test(value);
   } else {
-    throw new Error('options.test needs to be regex or function.');
+    throw new Error("options.test needs to be regex or function.");
   }
-}
+};
 
 module.exports = class DtsBundlePlugin {
-	constructor({ test = /\.tsx?$/, name = '[name].d.ts' } = {}) {
-		this.test = test;
-		this.name = name;
-	}
+  constructor({ test = /\.tsx?$/, name = "[name].d.ts" } = {}) {
+    this.test = test;
+    this.name = name;
+  }
 
-	apply(compiler) {
-		compiler.plugin('emit', (compilation, callback) => {
-			compilation.chunks.forEach((chunk) => {
-				for (const module of chunk.modulesIterable) {
-					if (module.issuer && module.issuer.depth === 0 && runTest(this.test, module.resource)) {
-						const dts = generateDtsBundle(module.resource);
-						const dtsName = loaderUtils.interpolateName(
-							{
-								resourcePath: `./${chunk.name}.js`,
-							},
-							this.name,
-							{},
-						);
+  apply(compiler) {
+    compiler.plugin("emit", (compilation, callback) => {
+      compilation.entrypoints.forEach(entrypoint => {
+        entrypoint.origins.forEach(element => {
+          if (runTest(this.test, element.request)) {
+            const dts = generateDtsBundle(element.request);
+            const name = this.name;
+            const dtsName = loaderUtils.interpolateName(
+              {
+                resourcePath: `./${name}.js`
+              },
+              this.name,
+              {}
+            );
 
-						compilation.assets[dtsName] = {
-							source() {
-								return dts;
-							},
-							size() {
-								return dts.length;
-							}
-						};
-					}
-        		}
-     		});
-      		callback();
-		});
-	}
+            compilation.assets[dtsName] = {
+              source() {
+                return dts;
+              },
+              size() {
+                return dts.length;
+              }
+            };
+          }
+        });
+      });
+
+      callback();
+    });
+  }
 };
