3cb291e10bef78db441409f9f1de3b0b94321849
Juan Camilo Salazar <juan.salazar@impartner.com>
2019-02-21 08:58:18 -0500

    added eslintrc.json to establish linter rules for the code

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-const { generateDtsBundle } = require("dts-bundle-generator");
-const loaderUtils = require("loader-utils");
+const { generateDtsBundle } = require('dts-bundle-generator');
+const loaderUtils = require('loader-utils');
 
 const runTest = (test, value) => {
-  if (test instanceof RegExp) {
-    return test.test(value);
-  } else if (typeof test === "function") {
-    return test(value);
-  } else {
-    throw new Error("options.test needs to be regex or function.");
-  }
+    if (test instanceof RegExp) {
+        return test.test(value);
+    } else if (typeof test === 'function') {
+        return test(value);
+    } else {
+        throw new Error('options.test needs to be regex or function.');
+    }
 };
 
 module.exports = class DtsBundlePlugin {
-  constructor({ test = /\.tsx?$/, name = "[name].d.ts" } = {}) {
-    this.test = test;
-    this.name = name;
-  }
+    constructor({ test = /\.tsx?$/, name = '[name].d.ts' } = {}) {
+        this.test = test;
+        this.name = name;
+    }
 
-  apply(compiler) {
-    compiler.plugin("emit", (compilation, callback) => {
-      compilation.entrypoints.forEach(entrypoint => {
-        entrypoint.origins.forEach(element => {
-          if (runTest(this.test, element.request)) {
-            const dts = generateDtsBundle(element.request);
-            const name = this.name;
-            const dtsName = loaderUtils.interpolateName(
-              {
-                resourcePath: `./${name}.js`
-              },
-              this.name,
-              {}
-            );
+    apply(compiler) {
+        compiler.plugin('emit', (compilation, callback) => {
+            compilation.entrypoints.forEach(entrypoint => {
+                entrypoint.origins.forEach(element => {
+                    if (runTest(this.test, element.request)) {
+                        const dts = generateDtsBundle(element.request);
+                        const name = this.name;
+                        const dtsName = loaderUtils.interpolateName(
+                            {
+                                resourcePath: `./${name}.js`
+                            },
+                            this.name,
+                            {}
+                        );
 
-            compilation.assets[dtsName] = {
-              source() {
-                return dts;
-              },
-              size() {
-                return dts.length;
-              }
-            };
-          }
+                        compilation.assets[dtsName] = {
+                            source() {
+                                return dts;
+                            },
+                            size() {
+                                return dts.length;
+                            }
+                        };
+                    }
+                });
+            });
+
+            callback();
         });
-      });
-
-      callback();
-    });
-  }
+    }
 };
