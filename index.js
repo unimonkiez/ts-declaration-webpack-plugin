fa5efad4b98ef876c213719b33ff22c286c3efd9
Yuval Saraf <unimonkiez@gmail.com>
2020-03-25 14:29:53 +0200

    Added eslint

--- index.js
+++ index.js
@@ -2,49 +2,50 @@ const { generateDtsBundle } = require('dts-bundle-generator');
 const loaderUtils = require('loader-utils');
 
 const runTest = (test, value) => {
-    if (test instanceof RegExp) {
-        return test.test(value);
-    } else if (typeof test === 'function') {
-        return test(value);
-    } else {
-        throw new Error('options.test needs to be regex or function.');
-    }
+  if (test instanceof RegExp) {
+    return test.test(value);
+  } if (typeof test === 'function') {
+    return test(value);
+  }
+  throw new Error('options.test needs to be regex or function.');
 };
 
 module.exports = class DtsBundlePlugin {
-    constructor({ test = /\.tsx?$/, name = '[name].d.ts' } = {}) {
-        this.test = test;
-        this.name = name;
-    }
+  constructor({ test = /\.tsx?$/, name = '[name].d.ts' } = {}) {
+    this.test = test;
+    this.name = name;
+  }
 
-    apply(compiler) {
-        compiler.plugin('emit', (compilation, callback) => {
-            compilation.entrypoints.forEach(entrypoint => {
-                entrypoint.origins.forEach(element => {
-                    if (runTest(this.test, element.request)) {
-                        const dts = generateDtsBundle([{filePath: element.request}]);
-                        const name = this.name;
-                        const dtsName = loaderUtils.interpolateName(
-                            {
-                                resourcePath: `./${name}.js`
-                            },
-                            this.name,
-                            {}
-                        );
+  apply(compiler) {
+    compiler.plugin('emit', (compilation, callback) => {
+      compilation.entrypoints.forEach((entrypoint) => {
+        entrypoint.origins.forEach((element) => {
+          if (runTest(this.test, element.request)) {
+            const dts = generateDtsBundle([{ filePath: element.request }]);
+            const { name } = this;
+            const dtsName = loaderUtils.interpolateName(
+              {
+                resourcePath: `./${name}.js`,
+              },
+              this.name,
+              {},
+            );
 
-                        compilation.assets[dtsName] = {
-                            source() {
-                                return dts;
-                            },
-                            size() {
-                                return dts.length;
-                            }
-                        };
-                    }
-                });
-            });
-
-            callback();
+            // The way webpack's api is designed
+            // eslint-disable-next-line no-param-reassign
+            compilation.assets[dtsName] = {
+              source() {
+                return dts;
+              },
+              size() {
+                return dts.length;
+              },
+            };
+          }
         });
-    }
+      });
+
+      callback();
+    });
+  }
 };
